991d96e2a363fd865dd918a2ac4365b9db6ee632
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2025-08-16 18:07:52 -0700

    updated libraries

--- resources/leiningen/new/lst/project.clj
+++ resources/leiningen/new/lst/project.clj
@@ -1,18 +1,18 @@
-(defproject sk "0.1.0"
+(defproject {{name}} "0.1.0"
   :description "{{name}}" ; Change me
   :url "http://example.com/FIXME" ; Change me - optional
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.clojure/data.csv "1.1.0"]
-                 [org.clojure/data.json "2.5.0"]
+                 [org.clojure/data.json "2.5.1"]
                  [org.slf4j/slf4j-simple "2.0.17"]
                  [compojure "1.7.1"]
-                 [hiccup "1.0.5"]
+                 [hiccup "2.0.0"]
                  [buddy/buddy-hashers "2.0.167"]
                  [com.draines/postal "2.0.5"]
                  [cheshire "6.0.0"]
-                 [clj-pdf "2.7.0"]
+                 [clj-pdf "2.7.4"]
                  [ondrs/barcode "0.1.0"]
                  [pdfkit-clj "0.1.7"]
                  [cljfmt "0.9.2"]
@@ -22,25 +22,25 @@
                  [org.clojure/java.jdbc "0.7.12"]
                  ;; Active JDBC drivers (MySQL, PostgreSQL, SQLite)
                  [mysql/mysql-connector-java "8.0.33"]
-                 [org.postgresql/postgresql "42.7.3"]
-                 [org.xerial/sqlite-jdbc "3.45.1.0"]
+                 [org.postgresql/postgresql "42.7.7"]
+                 [org.xerial/sqlite-jdbc "3.50.3.0"]
                  ;; Optional JDBC drivers (uncomment if needed)
                  ;; [com.microsoft.sqlserver/mssql-jdbc "12.8.1.jre11"]   ; SQL Server
                  ;; [com.h2database/h2 "2.2.224"]                        ; H2
                  ;; [com.oracle.database.jdbc/ojdbc8 "21.11.0.0"]        ; Oracle (check licensing)
                  [ragtime "0.8.1"]
-                 [ring/ring-core "1.14.1"]
-                 [ring/ring-jetty-adapter "1.14.1"]
+                 [ring/ring-core "1.14.2"]
+                 [ring/ring-jetty-adapter "1.14.2"]
                  [ring/ring-defaults "0.6.0"]
-                 [ring/ring-devel "1.14.1"]
+                 [ring/ring-devel "1.14.2"]
                  [ring/ring-codec "1.3.0"]]
-  :main ^:skip-aot {{name}}.core
-  :aot [{{name}}.core]
+  :main ^:skip-aot {{name}} .core
+  :aot [{{name}} .core]
   :plugins [[lein-ancient "0.7.0"]
             [lein-pprint "1.3.2"]]
   :uberjar-name "{{name}}.jar"
   :target-path "target/%s"
-  :ring {:handler {{name}}.core
+  :ring {:handler {{name}} .core
          :auto-reload? true
          :auto-refresh? false}
   :resources-paths ["shared" "resources"]
@@ -59,4 +59,4 @@
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
              :dev {:source-paths ["src" "dev"]
-                   :main {{name}}.dev}})
+                   :main {{name}} .dev}})
