3cefb884f85bbee1551530130f3354cafef9566d
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2025-12-17 10:19:28 -0800

    new version upgrade

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.hector/lein-template.lst "0.1.13"
+(defproject org.clojars.hector/lein-template.lst "0.1.14"
   :description "LST Skeleton Web App"
   :url "http://github.com/hectorqlucero/lst"
   :license {:name "MIT License"
